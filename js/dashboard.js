c5492db6503dc9dc220fa570d58a15de881c923b
digirabbithome <swimming8250@yahoo.com.tw>
2025-06-30 07:39:17 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,2 +1,11 @@
-// dashboard.js: handles dashboard UI
-console.log('dashboard.js loaded');
+// dashboard.js
+
+import { initializeFirebase, checkAuth } from './firebase.js';
+import { loadSidebar } from './sidebar.js';
+import { loadDailyTasks } from './dailyTasks.js';
+
+initializeFirebase();
+checkAuth().then(user => {
+  loadSidebar();
+  loadDailyTasks(user);
+});
