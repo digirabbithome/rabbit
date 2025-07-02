c6a054d51c8d39a4cbdfe9e25fc7eec5e51f765b
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-02 13:23:05 +0800

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,11 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
   authDomain: "rabbithome-auth.firebaseapp.com",
   projectId: "rabbithome-auth",
-  storageBucket: "rabbithome-auth.appspot.com",
+  storageBucket: "rabbithome-auth.firebasestorage.app",
   messagingSenderId: "50928677930",
   appId: "1:50928677930:web:e8eff13c8028b888537f53"
 };
@@ -13,11 +14,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-document.getElementById("loginButton").addEventListener("click", () => {
+window.login = function() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-
   signInWithEmailAndPassword(auth, email, password)
-    .then(() => window.location.href = "index.html")
-    .catch(err => alert("登入失敗：" + err.message));
-});
+    .then(() => {
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      alert("登入失敗：" + error.message);
+    });
+}
