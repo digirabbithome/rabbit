36922bfc4a3baf21e2ba41293e885090025e344b
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-02 22:41:39 +0800

    Delete sidebar.js

--- sidebar.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("testBtn");
-  if (btn) {
-    btn.addEventListener("click", () => alert("✅ JS 成功運作"));
-  }
-});
